086c4431a31b5c9616e26699b4921196f29f56f6
Павел <elantsev.pa@gmail.com>
2019-10-16 11:29:31 +0300

    + MessageList component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AddMessage from "./components/AddMessages";
+import MessagesList from "./components/MessagesList";
 
 function App() {
   return (
     <div id="container">
       {/* <Sidebar /> */}
       <section>
-        {/* <MessagesList /> */}
+        <MessagesList />
         <AddMessage />
       </section>
     </div>
---
+++ src/components/MessagesList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Message from "./Message";
+import { useSelector } from "react-redux";
+
+const MessageList = () => {
+  const messages = useSelector(state => state.messages);
+  return (
+    <section id="message-list">
+      <ul>
+        {messages.map(message => (
+          <Message key={message.id} {...message} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default MessageList;
